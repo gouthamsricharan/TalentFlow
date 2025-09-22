4f7c052413e62e3f0556b7f13828776d8d354ae3
Goutham <gouthamsricharan18@gmail.com>
2025-09-22 19:25:56 +0530

    initialization

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App'
+import { makeServer } from './mirage-server'
+import { initializeDatabase } from './db/index.js'
+
+async function startApp() {
+  // Wait for database to be ready
+  await initializeDatabase()
+  
+  
+  
+    makeServer()
+    console.log('✅ MirageJS server started')
+
+
+  // Render app
+  const rootElement = document.getElementById('root')
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
+}
+
+startApp()
